9e4c0a85db3d6d70a3ebd7396ece8fb1a059915a
kishorep-08 <kishore.pkdp@gmail.com>
2026-02-03 21:18:58 +0530

    jenkins

--- 40-ecr/main.tf
+++ 40-ecr/main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "component" {
   for_each = toset(var.component)
   name                 = "${var.project_name}/${each.value}"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = false
